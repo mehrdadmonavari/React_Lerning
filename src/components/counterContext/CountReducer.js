59ef8936d977bd37088860a6aa0c8ad852532ddf
mehrdad <mehrdadmd27@gmail.com>
2021-11-13 20:36:47 +0330

    improve example count reducer
    use multiple state and and use action of object (type, value)

--- src/components/counterContext/CountReducer.js
+++ src/components/counterContext/CountReducer.js
@@ -1,32 +1,107 @@
 import { useReducer } from "react";
 
-const initialState = 0;
+const initialState = { firstCount: 0, secondCount: 0 };
 
 const reducer = (state, action) => {
-  switch (action) {
-    case "IncrementOne":
-      return state + 1;
-      break;
-    case "IncrementFive":
-      return state + 5;
-      break;
-    case "DecrementOne":
-      return state - 1;
-      break;
+  switch (action.type) {
+    case "IncrementFirstCount":
+      return { ...state, firstCount: state.firstCount + action.value };
+    case "DecrementFirstCount":
+      return { ...state, firstCount: state.firstCount - action.value };
+    case "ResetFirstCount":
+      return { ...state, firstCount: initialState.firstCount };
+    case "IncrementSecondCount":
+      return { ...state, secondCount: state.secondCount + action.value };
+    case "DecrementSecondCount":
+      return { ...state, secondCount: state.secondCount - action.value };
+    case "ResetSecondCount":
+      return { ...state, secondCount: initialState.secondCount };
     default:
-      return state;
+      return { state };
   }
 };
 
 const CountReducer = () => {
   const [count, countDispatch] = useReducer(reducer, initialState);
 
-  return <div>
-    <div>count is : {count}</div>
-    <button onClick={() => {countDispatch("IncrementOne")}}>add One</button>
-    <button onClick={() => {countDispatch("IncrementFive")}}>add Five</button>
-    <button onClick={() => {countDispatch("DecrementOne")}}>decrement One</button>
-  </div>;
+  return (
+    <div>
+      <div className="row border border-secondary rounded mx-5 my-3 p-3">
+        <div className="col-md-3 pt-1 text-center">
+          count is : {count.firstCount}
+        </div>
+        <div className="col-md-6 ml-5 text-center">
+          <button
+            className="btn btn-outline-info mx-2"
+            onClick={() =>
+              countDispatch({ type: "IncrementFirstCount", value: 1 })
+            }
+          >
+            addOne
+          </button>
+          <button
+            className="btn btn-outline-info mx-2"
+            onClick={() =>
+              countDispatch({ type: "IncrementFirstCount", value: 5 })
+            }
+          >
+            addFive
+          </button>
+          <button
+            className="btn btn-outline-danger mx-2"
+            onClick={() =>
+              countDispatch({ type: "DecrementFirstCount", value: 1 })
+            }
+          >
+            decrement
+          </button>
+          <button
+            className="btn btn-outline-secondary mx-2"
+            onClick={() => countDispatch({ type: "ResetFirstCount" })}
+          >
+            reset
+          </button>
+        </div>
+      </div>
+      <div className="row border border-secondary rounded mx-5 my-3 p-3">
+        <div className="col-md-3 pt-1 text-center">
+          count is : {count.secondCount}
+        </div>
+        <div className="col-md-6 ml-5 text-center">
+          <button
+            className="btn btn-outline-info mx-2"
+            onClick={() =>
+              countDispatch({ type: "IncrementSecondCount", value: 1 })
+            }
+          >
+            addOne
+          </button>
+          <button
+            className="btn btn-outline-info mx-2"
+            onClick={() =>
+              countDispatch({ type: "IncrementSecondCount", value: 1 })
+            }
+          >
+            addFive
+          </button>
+          <button
+            className="btn btn-outline-danger mx-2"
+            onClick={() =>
+              countDispatch({ type: "IncrementSecondCount", value: 1 })
+            }
+          >
+            decrement
+          </button>
+          <button
+            className="btn btn-outline-secondary mx-2"
+            onClick={() => countDispatch({ type: "ResetSecondCount" })}
+          >
+            reset
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CountReducer;
